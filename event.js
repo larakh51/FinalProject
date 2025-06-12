191c4404ab789aa470e677e064f165e8cdfb97dc
larakh51 <larakh51@gmail.com>
2025-06-12 19:50:59 +0300

    Update event.js

--- event.js
+++ event.js
@@ -6,6 +6,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const popup = document.getElementById("popupOverlay");
   const form = document.getElementById("contactForm");
   const detailsPopup = document.getElementById("detailsPopup");
+  
+  searchInput.addEventListener("input", () => {
+    const query = searchInput.value.toLowerCase();
+    const filtered = contacts.filter((c) =>
+      c.name.toLowerCase().includes(query)
+    );
+    renderContacts(filtered);
+  }); 
+
+  deleyeAllBtn.addEventListener("click",() =>{
+    if(confirm("Are you sure you want to delete all contacts?"){
+      contacts.length = 0;
+      renderContacts([]);
+    }
+  });
+
+  
+                                
 
   
 
