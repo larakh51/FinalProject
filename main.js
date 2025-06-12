d7d29d51e72be7e36479d963062f3997375803d7
larakh51 <larakh51@gmail.com>
2025-06-12 15:03:35 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  renderContacts(contacts);
+});
+
+function renderContact(list){
+  const container = document.getElementByld
+  ("contactsContainer");
+  container.innerHTML = "";
