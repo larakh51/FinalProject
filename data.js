cdb1c8d11b842cef5d70f5993b0599624a9ac722
larakh51 <larakh51@gmail.com>
2025-06-12 14:31:06 +0300

    Update data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const contacts = [
+  {
+    name: "Laura Sbeit",
+    phone: "052-1234567",
+    address: "jerusalem",
+    age: 24,
+    imageUrl: "https://i.pravatar.cc/200?img=5",
+  },
+  {
+    name: "Andro Levi",
+    phone: "054-7654321",
+    address: "Haifa",
+    age: 28,
+    imageUrl: "https://i.pravatar.cc/200?img=12",
+  },
+  {
+    name: "Jan Salman",
+    phone: "050-8765432",
+    address: "Nazareth",
+    age: 26,
+    imageUrl: "https://i.pravatar.cc/200?img=8",
+  },
+    
+    
+    
+    
+    
