4ac6f644143a1c2e48faa1e84a9ea77e93c9e336
larakh51 <larakh51@gmail.com>
2025-06-12 15:52:19 +0300

    Update event.js

---
+++ event.js
@@ -0,0 +1,13 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const searchInput = document.getElementById("searchInput");
+  const deleteAllBtn = document.getElementById("deleteAllBtn");
+  const addBtn = document.getElementByld("addContactBtn");
+  const popup = document.getElementById("popupOverlay");
+  const form = document.getElementById("contactForm");
+  const detailsPopup = document.getElementById("detailsPopup");
+
+  
+
+
+                  
