f16f3c8d8fd28ab87f433a9c7ad82e700e8b640f
larakh51 <larakh51@gmail.com>
2025-06-12 20:14:30 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,4 +13,19 @@ function renderContact(list){
     const card = document.createElement("div");
     card.className = "contact-card";
 
+    card.innerHTML =  `
+      <img class="avatar" src="${contact.imageUrl}" alt="${contact.name}">
+      <div class="info">
+        <div class="contact-name">${contact.name}</div>
+        <div class="contact-phone">📞 ${contact.phone}</div>
+        <div class="card-buttons">
+          <button class="view-btn" data-id="${index}">ℹ️</button>
+          <button class="edit-btn" data-id="${index}">✏️</button>
+          <button class="delete-btn" data-id="${index}">🗑️</button>
+        </div>
+      </div>
+    `;
+
+      
+
     
