914fa3f00b1945fc14894eb20df654ee2ae25d3b
larakh51 <larakh51@gmail.com>
2025-06-18 11:22:53 +0300

    Update event.js

--- event.js
+++ event.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const searchInput = document.getElementById("searchInput");
   const deleteAllBtn = document.getElementById("deleteAllBtn");
-  const addBtn = document.getElementByld("addContactBtn");
+  const addBtn = document.getElementById("addContactBtn");
   const popup = document.getElementById("popupOverlay");
   const form = document.getElementById("contactForm");
   const detailsPopup = document.getElementById("detailsPopup");
