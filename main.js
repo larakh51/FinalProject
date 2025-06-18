50299a04d78cf4e0561460f25ce8185827411fcb
larakh51 <larakh51@gmail.com>
2025-06-18 11:26:50 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,8 @@ document.addEventListener("DOMContentLoaded", () => {
   renderContacts(contacts);
 });
 
-function renderContact(list){
-  const container = document.getElementByld
-  ("contactsContainer");
+function renderContacts(list) {
+  const container = document.getElementById("contactsContainer");
   container.innerHTML = "";
 
   list.forEach((contact, index) => {
@@ -15,24 +14,27 @@ function renderContact(list){
       contact.birthDate &&
       new Date(contact.birthDate).getDate() === today.getDate() &&
       new Date(contact.birthDate).getMonth() === today.getMonth();
-    const birthdayIcon = isBirthday ? '<span class="birthday">🎂</span>' : '';
+    const birthdayIcon = isBirthday ? '<span class="birthday">🎂</span>' : "";
+  
 
     const card = document.createElement("div");
     card.className = "contact-card";
-       card.className = "contact-card";
     card.addEventListener("mouseover", () => {
       card.classList.add("hovered");
     });
     card.addEventListener("mouseout", () => {
       card.classList.remove("hovered");
     });
-    const tagLabel = contact.tag ? `<span class="tag-label ${contact.tag}">${contact.tag}</span>` : '';
+    const tagLabel = contact.tag
+      ? `<span class="tag-label ${contact.tag}">${contact.tag}</span>`
+      : "";
 
 
-    card.innerHTML =  `
+    card.innerHTML = `
       <img class="avatar" src="${contact.imageUrl}" alt="${contact.name}">
       <div class="info">
-        <div class="contact-name">${contact.name}${tagLabel}${birthdayIcon}</div>
+        <div class="contact-name">${contact.name} ${tagLabel}${birthdayIcon}</div>
+
         <div class="contact-phone">📞 ${contact.phone}</div>
         <div class="card-buttons">
           <button class="view-btn" data-id="${index}">ℹ️</button>
@@ -48,11 +50,9 @@ function renderContact(list){
   updatePeopleCount();
 }
 
-function updatePeopleCount(){
-  document.getElementByld("peopleCount").textContent = contacts.length;
+function updatePeopleCount() {
+  document.getElementById("peopleCount").textContent = contacts.length;
 }
-
-
 
 
 
